b8002dd0b659b1ed2c219f893cfa3ae1864559d7
Ednakrz <ednakrz@lcg.unam.mx>
2025-02-06 08:52:34 -0600

    Creación de mi del objeto rse_gene_SRP095512, con el ideentificador de mi proyecto de interés

--- Scrip_Project.R
+++ Scrip_Project.R
@@ -3,3 +3,19 @@ library("recount3")
 
 ## Revisemos todos los proyectos con datos de humano en recount3
 human_projects <- available_projects()
+
+
+## Identificador de mi proyecto de interes:SRP095512
+##Obtencion del proyecto de interes
+proj_info <- subset(
+  human_projects,
+  project == "SRP095512" & project_type == "data_sources"
+)
+
+
+## Crea un objeto de tipo RangedSummarizedExperiment (RSE)
+## con la información a nivel de genes
+rse_gene_SRP095512 <- create_rse(proj_info)
+
+## Explora el objeto RSE
+rse_gene_SRP095512
