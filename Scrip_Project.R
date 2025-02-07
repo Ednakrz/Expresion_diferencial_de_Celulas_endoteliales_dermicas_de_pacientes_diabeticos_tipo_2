0024ce5715b98bcf18fdebff640ea6069fed0229
Ednakrz <ednakrz@lcg.unam.mx>
2025-02-06 20:22:14 -0600

    Hice el código completo del análisis de expresión diferencial

--- Scrip_Project.R
+++ Scrip_Project.R
@@ -45,4 +45,138 @@ summary(as.data.frame(colData(rse_gene_SRP095512)[
 ]))
 
 
+# Check quality of the data
+rse_gene_SRP095512$assigned_gene_prop <-
+  rse_gene_SRP095512$recount_qc.gene_fc_count_all.assigned /
+  rse_gene_SRP095512$recount_qc.gene_fc_count_all.total
+summary(rse_gene_SRP095512$assigned_gene_prop)
+
+# Observe differences among samples injected with different substances
+with(colData(rse_gene_SRP095512),
+     tapply(assigned_gene_prop,
+            sra_attribute.disease_state,
+            summary))
+
+# Plot the distribution of the assigned gene proportion
+hist(rse_gene_SRP095512$assigned_gene_prop, col = "orange")
+
+# Save the unfiltered data
+rse_gene_SRP095512_unfiltered <- rse_gene_SRP095512
+
+## ------------------- Normalizacion de los datos-------------------------------
+
+library("edgeR") # BiocManager::install("edgeR", update = FALSE)
+dge <- DGEList(
+  counts = assay(rse_gene_SRP095512, "counts"),
+  genes = rowData(rse_gene_SRP095512)
+)
+dge <- calcNormFactors(dge)
+
+
+## ---------------- Expresion diferencial --------------------------------------
+
+library("ggplot2")
+ggplot(as.data.frame(colData(rse_gene_SRP095512)), aes(y = assigned_gene_prop, x = sra_attribute.disease_state)) +
+  geom_boxplot() +
+  theme_bw(base_size = 20) +
+  ylab("Assigned Gene Prop") +
+  xlab("Gender")
+
+# Modelo estadístico
+
+mod <- model.matrix(~ sra_attribute.gender +sra_attribute.disease_state + assigned_gene_prop,
+                    data = colData(rse_gene_SRP095512)
+)
+colnames(mod)
+
+## Usamos limma para realizar el análisis de expresión diferencial
+library("limma")
+vGene <- voom(dge, mod, plot = TRUE)
+
+eb_results <- eBayes(lmFit(vGene))
+
+de_results <- topTable(
+  eb_results,
+  coef = 2,
+  number = nrow(rse_gene_SRP095512),
+  sort.by = "none"
+)
+dim(de_results)
+
+## Genes diferencialmente expresados entre pre y post natal con FDR < 5%
+table(de_results$adj.P.Val < 0.05)
+
+## Visualicemos los resultados estadísticos
+plotMA(eb_results, coef = 3)
+
+volcanoplot(eb_results, coef = 2, highlight = 3, names = de_results$gene_name)
+de_results[de_results$gene_name %in% c("TTY14", "RPS4Y1", "RP11-331"), ]
+
+
+volcanoplot(eb_results, coef = 3, highlight = 3, names = de_results$gene_name)
+de_results[de_results$gene_name %in% c("WNK2", "NUD111", "FAM83H"), ]
+
+
+## Visualizando la expresion diferencial a traves de un Heatmap------
+
+## Extraer valores de los genes de interés
+exprs_heatmap <- vGene$E[rank(de_results$adj.P.Val) <= 50, ]
+
+## Creemos una tabla con información de las muestras
+## y con nombres de columnas más amigables
+df <- as.data.frame(colData(rse_gene_SRP095512)[, c("sra_attribute.gender", "sra_attribute.disease_state")])
+colnames(df) <- c( "Gender", "disease_state")
+
+## Hagamos un heatmap
+library("pheatmap")
+pheatmap(
+  exprs_heatmap,
+  cluster_rows = TRUE,
+  cluster_cols = TRUE,
+  show_rownames = FALSE,
+  show_colnames = FALSE,
+  annotation_col = df
+)
+
+pheatmap(
+  exprs_heatmap,
+  cluster_rows = TRUE,
+  cluster_cols = TRUE,
+  show_rownames = FALSE,
+  show_colnames = FALSE,
+  annotation_col = df,
+  scale = "row"
+)
+
+ComplexHeatmap::pheatmap(
+  exprs_heatmap,
+  cluster_rows = TRUE,
+  cluster_cols = TRUE,
+  show_rownames = FALSE,
+  show_colnames = FALSE,
+  annotation_col = df,
+  scale = "row"
+)
+## Para colores
+library("RColorBrewer")
+
+## Conviertiendo los valores de Gender a colores
+col.gender <- df$sra_attribute.gender
+levels(col.gender) <- brewer.pal(nlevels(col.gender), "Dark2")
+
+col.gender <- as.character(col.gender)
+
+## MDS por genero
+plotMDS(vGene$E, labels = df$sra_attribute.gender, col = col.gender)
+
+## Conviertiendo los valores de Disease a colores
+col.disease <- df$sra_attribute.disease_state
+levels(col.disease) <- brewer.pal(nlevels(col.disease), "Dark2")
+
+col.disease <- as.character(col.disease)
+
+## MDS por genero
+plotMDS(vGene$E, labels = df$sra_attribute.disease_state, col = col.disease)
+
+
 
